cee8b2d38f4efa803f92c12e59b17c71c52d2db0
Matthias Nippert <matthias.nippert@gmail.com>
2022-12-22 10:57:53 +0100

    First tryout round with custom integrations

--- custom_components/stiebel_eltron_isg/__init__.py
+++ custom_components/stiebel_eltron_isg/__init__.py
@@ -82,6 +82,15 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
+#MNI TODO: Put somewhere meaningful
+ISG_TYPE_2 = 2   #16bit signed,   scale 0.1
+ISG_TYPE_6 = 6   #16bit unsigned, scale 1
+ISG_TYPE_7 = 7   #16bit signed,   scale 0.01
+ISG_TYPE_8 = 8   #8bit  unsigned, scale 1
+ISG_TYPE_C = 128 #composite value: multiple values as list
+
+
+
 async def async_setup(hass: HomeAssistant, config: Config):
     """Set up this integration using YAML is not supported."""
     return True
@@ -165,11 +174,23 @@ class StiebelEltronModbusDataCoordinator(DataUpdateCoordinator):
             raise UpdateFailed() from exception
 
     def read_modbus_data(self) -> Dict:
-        result = {
-            **self.read_modbus_energy(),
-            **self.read_modbus_system_state(),
-            **self.read_modbus_system_values(),
-        }
+        #MNI TODO: Cache result of device identification. It wont change.
+        id = self.read_modbus_controller_id()
+
+        if 103 == id:
+            result = {
+                #**self.read_modbus_energy(),
+                #**self.read_modbus_system_state(),
+                **self.read_modbus_system_values_lw(),
+                "controller_id": id,
+            }
+        else:
+            result = {
+                **self.read_modbus_energy(),
+                **self.read_modbus_system_state(),
+                **self.read_modbus_system_values(),
+                "controller_id": id,
+            }
         return result
 
     def read_modbus_system_state(self) -> Dict:
@@ -190,6 +211,103 @@ class StiebelEltronModbusDataCoordinator(DataUpdateCoordinator):
 
             result[IS_SUMMER_MODE] = (state & (1 << 7)) != 0
             result[IS_COOLING] = (state & (1 << 8)) != 0
+
+        return result
+    
+    def parse_modbus_values_by_map(self, values_map:list, decoder:BinaryPayloadDecoder) -> Dict:
+        """Decode modbus registers into Dict by a value map laying out the type of each value."""
+        result = {}
+
+        for (valname, valtype) in values_map:
+            #If multiple values need to be combined, parser will return all of them as list to be combined after parsing
+            composite_val = False
+            if valtype & ISG_TYPE_C:
+                composite_val = True
+                valtype &=~ ISG_TYPE_C
+
+            if ISG_TYPE_2 == valtype:
+                value = get_isg_scaled_value(
+                    decoder.decode_16bit_int()
+                )
+            elif ISG_TYPE_6 == valtype:
+                value = decoder.decode_16bit_uint()
+            else:
+                raise ValueError("Unknown register type!")
+            
+            if valname != None:
+                if composite_val:
+                    result.setdefault(valname, []).append(value)
+                else:
+                    result[valname] = value
+
+        return result
+
+    def read_modbus_system_values_LW(self) -> Dict:
+        ACTUAL_HUMIDITY_HK1 = "actual_humidity_hk1"
+        ACTUAL_HUMIDITY_HK2 = "actual_humidity_hk2"
+        ACTUAL_CIRCUIT_TEMPERATURE_HK1 = "actual_temperature_hk1"
+        TARGET_CIRCUIT_TEMPERATURE_HK1 = "target_temperature_hk1"
+        ACTUAL_CIRCUIT_TEMPERATURE_HK2 = "actual_temperature_hk2"
+        TARGET_CIRCUIT_TEMPERATURE_HK2 = "target_temperature_hk2"
+        SUPPLY_TEMPERATURE = "supply_temperature"
+        RETURN_TEMPERATURE = "return_temperature"
+        PRESSURE_CIRCUIT = "pressure_circuit"
+        COMPRESSOR_STARTS = "compressor_starts"
+
+        system_values_map_LW = [
+            [ACTUAL_TEMPERATURE_HK1, ISG_TYPE_2],
+            [TARGET_TEMPERATURE_HK1, ISG_TYPE_2],
+            [ACTUAL_HUMIDITY_HK1, ISG_TYPE_2],
+            [ACTUAL_TEMPERATURE_HK2, ISG_TYPE_2],
+            [ACTUAL_TEMPERATURE_HK2, ISG_TYPE_2],
+
+            [ACTUAL_HUMIDITY_HK2, ISG_TYPE_2],
+            [OUTDOOR_TEMPERATURE, ISG_TYPE_2],
+            [ACTUAL_CIRCUIT_TEMPERATURE_HK1, ISG_TYPE_2],
+            [TARGET_CIRCUIT_TEMPERATURE_HK1, ISG_TYPE_2],
+            [ACTUAL_CIRCUIT_TEMPERATURE_HK2, ISG_TYPE_2],
+
+            [TARGET_CIRCUIT_TEMPERATURE_HK2, ISG_TYPE_2],
+            [SUPPLY_TEMPERATURE, ISG_TYPE_2],
+            [RETURN_TEMPERATURE, ISG_TYPE_2],
+            [PRESSURE_CIRCUIT, ISG_TYPE_2],
+            [None, ISG_TYPE_2], #volume_flow
+
+            [ACTUAL_TEMPERATURE_WATER, ISG_TYPE_2],
+            [TARGET_TEMPERATURE_WATER, ISG_TYPE_2],
+            [None, ISG_TYPE_6], #inlet_actual_fan_speed
+            [None, ISG_TYPE_6], #inlet_target_volume_flow
+            [None, ISG_TYPE_6], #outlet_actual_fan_speed
+
+            [None, ISG_TYPE_6], #outlet_target_volume_flow
+            [None, ISG_TYPE_6], #outlet_humidity
+            [None, ISG_TYPE_2], #outlet_temperature
+            [None, ISG_TYPE_2], #outlet_dew_point
+            [None, ISG_TYPE_2], #hk1_dew_point
+
+            [None, ISG_TYPE_2], #hk2_dew_point
+            [None, ISG_TYPE_2], #collector_temperature
+            [None, ISG_TYPE_2], #hot_gas_temperature
+            [None, ISG_TYPE_7], #high_pressure
+            [None, ISG_TYPE_7], #low_pressure
+
+            [COMPRESSOR_STARTS, ISG_TYPE_6|ISG_TYPE_C], #compressor_starts (documented differently, but this is only the high part: *1000)
+            [None, ISG_TYPE_2], #compressor_rpm
+            [None, ISG_TYPE_6], #mixed_water_volume
+            [COMPRESSOR_STARTS, ISG_TYPE_6|ISG_TYPE_C], #compressor_starts (undocumented, but this seems to be the low part: [0..999])
+        ]
+
+        result = {}
+        inverter_data = self.read_input_registers(slave=1, address=0, count=34)
+        if not inverter_data.isError():
+            decoder = BinaryPayloadDecoder.fromRegisters(
+                inverter_data.registers, byteorder=Endian.Big
+            )
+
+        result = self.parse_modbus_values_by_map(system_values_map_LW, decoder)
+
+        #Convert composite value to single number
+        result[COMPRESSOR_STARTS] = result[COMPRESSOR_STARTS][0]*1000 + result[COMPRESSOR_STARTS][1]
 
         return result
 
@@ -290,6 +408,16 @@ class StiebelEltronModbusDataCoordinator(DataUpdateCoordinator):
                 consumed_water_total_high * 1000 + consumed_water_total_low
             )
         return result
+    
+    def read_modbus_controller_id(self) -> int:
+        controller_id = 0
+        inverter_data = self.read_input_registers(slave=1, address=5001, count=1)
+        if not inverter_data.isError():
+            decoder = BinaryPayloadDecoder.fromRegisters(
+                inverter_data.registers, byteorder=Endian.Big
+            )
+            controller_id = decoder.decode_16bit_uint()
+        return controller_id
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
